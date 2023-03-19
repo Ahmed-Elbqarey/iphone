1565d5a0502eda7a238184e8feece360062c0752
ahmed elbqarey <amedo56734@gmail.com>
2023-03-19 16:58:28 +0200

    upload file

---
+++ main.js
@@ -0,0 +1,9 @@
+let phone = document.querySelector(".phone");
+let body = document.getElementById("body");
+function phoneColor(src) {
+  phone.src = src;
+}
+ 
+function colors(color){
+  body.style.background = color;
+}
